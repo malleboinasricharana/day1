4234e7ecbdbfa021dd6f848e93e80eb0d2a7de7e
GadagotiMamatha <gadagotimamatha@gmail.com>
2025-07-01 10:52:18 +0530

    Update max.py

--- max.py
+++ max.py
@@ -1,6 +1,9 @@
-a=[1,4,7,2,6]
-max=a[0]
+a=[1,2,6,7,3,4]
+first=second=a[0]
 for i in range(len(a)):
-    if(a[i]>max):
-        max=a[i]
-print(max)        
+if(a[i]>first):
+    second=first
+    first=a[i]
+elif(a[i]>second and a[i]<first):
+    second
+print(second)
